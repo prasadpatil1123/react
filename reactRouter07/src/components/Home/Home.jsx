0a6f00a1b06cf8459b95cf44465c7464db2c645a
prasadpatil1123 <122286888+prasadpatil1123@users.noreply.github.com>
2023-10-16 11:28:01 +0530

    Mon

--- reactRouter07/src/components/Home/Home.jsx
+++ reactRouter07/src/components/Home/Home.jsx
@@ -36,12 +36,12 @@ function Home() {
           <img className="w-96" src="https://i.ibb.co/5BCcDYB/Remote2.png" alt="image1" />
         </div>
       </aside>
-
+{/* 
       <div className="grid  place-items-center sm:mt-20">
         <img className="sm:w-96 w-48" src="https://i.ibb.co/2M7rtLk/Remote1.png" alt="image2" />
-      </div>
+      </div> */}
 
-      <h1 className="text-center text-2xl sm:text-5xl py-10 font-medium">Lorem Ipsum Yojo</h1>
+      {/* <h1 className="text-center text-2xl sm:text-5xl py-10 font-medium">Lorem Ipsum Yojo</h1> */}
     </div>
   )
 }
